6401f91bdbff61d0359f6a14d73dc63286046af3
boasmeier <62314872+boasmeier@users.noreply.github.com>
2022-05-08 14:13:12 +0200

    feat: add state filtering for images

--- src/app/components/images/images.component.ts
+++ src/app/components/images/images.component.ts
@@ -17,6 +17,8 @@ export class ImagesComponent implements AfterViewInit {
     images: MyImage[] = [];
     selectedImage?: MyImage;
     imageUploadActive: boolean;
+    filterActive: boolean;
+    filters: Map<string, boolean>;
 
     displayedColumns: string[] = ['idimage', 'name', 'mimetype', 'status'];
 
@@ -26,6 +28,12 @@ export class ImagesComponent implements AfterViewInit {
     constructor(private imageService: ImageService,
         private messageService: MessageService, private route: ActivatedRoute, private itr: ImageIdIterator) {
             this.imageUploadActive = false;
+            this.filterActive = false;
+            this.filters = new Map<string, boolean>();
+            this.filters.set('uploadedFilterButton', true);
+            this.filters.set('processedFilterButton', true);
+            this.filters.set('approvedFilterButton', true);
+            this.filters.set('exportedFilterButton', true);
     }
 
     onSelect(image: MyImage): void {
@@ -41,14 +49,15 @@ export class ImagesComponent implements AfterViewInit {
         const iddataset = Number(this.route.snapshot.paramMap.get('iddataset'));
         this.imageService.getImagesWithState().subscribe(images => {
             this.images = images.filter(i => i.iddataset == iddataset);
-            
-            // sort by id ascending
-            this.images.sort((i1, i2) => i1.idimage < i2.idimage ? -1 : 1 );
-
+            this.applyFilters();
             this.initImageIdIterator();
-            this.dataSource = new MatTableDataSource<MyImage>(this.images);
-            this.dataSource.paginator = this.paginator;
+            this.initMatPaginator();
         });
+    }
+
+    private initMatPaginator(): void {
+        this.dataSource = new MatTableDataSource<MyImage>(this.images);
+        this.dataSource.paginator = this.paginator;
     }
 
     private initImageIdIterator(): void {
@@ -69,5 +78,65 @@ export class ImagesComponent implements AfterViewInit {
 
     toggleImageUpload() {
         this.imageUploadActive = !this.imageUploadActive;
+    }
+
+    toggleFilter() {
+        this.filterActive = !this.filterActive;
+    }
+
+    toggleUploadedFilter() {
+        this.toggleFilterButton('uploadedFilterButton');
+    }
+
+    toggleProcessedFilter() {
+        this.toggleFilterButton('processedFilterButton');
+    }
+
+    toggleApprovedFilter() {
+        this.toggleFilterButton('approvedFilterButton');
+    }
+
+    toggleExportedFilter() {
+        this.toggleFilterButton('exportedFilterButton');
+    }
+
+    private toggleFilterButton(buttonId: string) {
+        const filterButton = document.getElementById(buttonId);
+
+        if(!filterButton) {
+            this.messageService.warn(`${buttonId} not found`);
+            return;
+        }
+
+        if(this.filters.get(buttonId)) { 
+            filterButton.classList.remove("btn-primary");
+            filterButton.classList.add("btn-secondary");
+        }
+        else{
+            filterButton.classList.remove("btn-secondary");
+            filterButton.classList.add("btn-primary");
+        }
+
+        this.filters.set(buttonId, !this.filters.get(buttonId));
+
+        this.getImages();
+    }
+
+    private applyFilters(): void {
+        if(!this.filters.get('uploadedFilterButton')) {
+            this.images = this.images.filter(i => i.state != 'Hochgeladen');
+        }
+        if(!this.filters.get('processedFilterButton')) {
+            this.images = this.images.filter(i => i.state != 'Erkannt');
+        }
+        if(!this.filters.get('approvedFilterButton')) {
+            this.images = this.images.filter(i => i.state != 'Bestaetigt');
+        }
+        if(!this.filters.get('exportedFilterButton')) {
+            this.images = this.images.filter(i => i.state != 'Exportiert');
+        }
+
+        // sort by id ascending
+        this.images.sort((i1, i2) => i1.idimage < i2.idimage ? -1 : 1 );
     }
 }
