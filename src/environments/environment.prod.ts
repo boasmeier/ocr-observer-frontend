a901970876f07e0c06b5bf0239c5e2ab53e59a14
boasmeier <62314872+boasmeier@users.noreply.github.com>
2022-05-09 20:19:33 +0200

    chore: use https in prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://wipro-fs22-bmeier.enterpriselab.ch:8080/api/v0'
+  baseUrl: 'https://wipro-fs22-bmeier.enterpriselab.ch/api/v0'
 };
